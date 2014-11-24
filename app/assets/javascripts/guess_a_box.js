5b244dab0d76b057220ad1d7bb1bffef7b08b360
Jay Wengrow <jaywngrw@gmail.com>
2014-11-24 16:35:15 -0600

    changing box color in guess-a-box game

--- app/assets/javascripts/guess_a_box.js
+++ app/assets/javascripts/guess_a_box.js
@@ -1,10 +1,11 @@
 var winningBox = Math.floor((Math.random() * 3)); // Picks a random number between 0 and 2:
 
-function guessBox(boxGuessed) {
-
+function guessBox(boxGuessed, box) {
   if (boxGuessed === winningBox) {
+    box.className += " right";
     alert("You win!");
   } else {
+    box.className += " wrong";
     console.log("wrong");
   }
 
